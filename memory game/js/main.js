73e102f09b5ae8b333df7f493caa74c047cb3dd8
Jonasdegroote1 <61138704+Jonasdegroote1@users.noreply.github.com>
2023-04-18 14:19:47 +0200

    finishing game met js

--- memory game/js/main.js
+++ memory game/js/main.js
@@ -15,8 +15,9 @@ const state = {
   loop: null
 }
 
+const dimensions = selectors.board.getAttribute('data-dimension')
+
 const generateGame = () => {
-  const dimensions = selectors.board.getAttribute('data-dimension')
 
   if (dimensions % 2 !== 0) {
     throw new Error("The dimension of the board must be an even number.")
@@ -69,5 +70,78 @@ const shuffle = array => {
   return clonedArray
 }
 
+const attachEventListeners = () => {
+  document.addEventListener('click', event => {
+    const eventTarget = event.target
+    const eventParent = eventTarget.parentElement
+
+    if (eventTarget.className.includes('card') && !eventParent.className.includes('flipped')) {
+      flipCard(eventParent)
+    } else if (eventTarget.nodeName === 'BUTTON' && !eventTarget.className.includes('disabled')) {
+      startGame()
+    }
+  })
+}
+
+const startGame = () => {
+  state.gameStarted = true
+  selectors.start.classList.add('disabled')
+
+  state.loop = setInterval(() => {
+    state.totalTime++
+
+    selectors.moves.innerText = `${state.totalFlips} moves`
+    selectors.timer.innerText = `time: ${state.totalTime} sec`
+  }, 1000)
+}
+
+const flipCard = card => {
+  state.flippedCards++
+  state.totalFlips++
+
+  if (!state.gameStarted) {
+    startGame()
+  }
+
+  if (state.flippedCards <= 2) {
+    card.classList.add('flipped')
+  }
+
+  if (state.flippedCards === 2) {
+    const flippedCards = document.querySelectorAll('.flipped:not(.matched)')
+
+    if (flippedCards[0].innerText === flippedCards[1].innerText) {
+      flippedCards[0].classList.add('matched')
+      flippedCards[1].classList.add('matched')
+      setTimeout(() => {
+        checkWinGames()
+      }, 600)
+
+    }
+
+    setTimeout(() => {
+      flipBackCards()
+    }, 1000)
+  }
+}
+
+const flipBackCards = () => {
+  document.querySelectorAll('.card:not(.matched)').forEach(card => {
+    card.classList.remove('flipped')
+  })
+  state.flippedCards = 0
+}
+
+const checkWinGames = () => {
+  const allCards = document.querySelectorAll('.flipped.matched');
+  if (allCards.length === dimensions * 2) {
+    winGame();
+  }
+}
+
+const winGame = () => {
+  alert("Congratulations, you won the game!");
+}
 
 generateGame()
+attachEventListeners()

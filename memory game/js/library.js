b5899d95342fe0c1a8446b24703f889858ae1a6b
Jonasdegroote1 <61138704+Jonasdegroote1@users.noreply.github.com>
2023-04-20 13:20:13 +0200

    adding library file cleaning up code

---
+++ memory game/js/library.js
@@ -0,0 +1,41 @@
+const pickRandom = (array, items) => {
+  const clonedArray = [...array]
+  const randomPicks = []
+
+  for (let index = 0; index < items; index++) {
+    const randomIndex = Math.floor(Math.random() * clonedArray.length)
+
+    randomPicks.push(clonedArray[randomIndex])
+    clonedArray.splice(randomIndex, 1)
+  }
+
+  return randomPicks
+}
+
+
+const shuffle = array => {
+  const clonedArray = [...array]
+
+  for (let index = clonedArray.length - 1; index > 0; index--) {
+    const randomIndex = Math.floor(Math.random() * (index + 1))
+    const original = clonedArray[index]
+
+    clonedArray[index] = clonedArray[randomIndex]
+    clonedArray[randomIndex] = original
+  }
+
+  return clonedArray
+}
+
+const attachEventListeners = () => {
+  document.addEventListener('click', event => {
+    const eventTarget = event.target
+    const eventParent = eventTarget.parentElement
+
+    if (eventTarget.className.includes('card') && !eventParent.className.includes('flipped')) {
+      flipCard(eventParent)
+    } else if (eventTarget.nodeName === 'BUTTON' && !eventTarget.className.includes('disabled')) {
+      startGame()
+    }
+  })
+}
--- memory game/js/main.js
+++ memory game/js/main.js
@@ -42,47 +42,6 @@ const generateGame = () => {
   selectors.board.replaceWith(parser.querySelector('.board'))
 }
 
-const pickRandom = (array, items) => {
-  const clonedArray = [...array]
-  const randomPicks = []
-
-  for (let index = 0; index < items; index++) {
-    const randomIndex = Math.floor(Math.random() * clonedArray.length)
-
-    randomPicks.push(clonedArray[randomIndex])
-    clonedArray.splice(randomIndex, 1)
-  }
-
-  return randomPicks
-}
-
-const shuffle = array => {
-  const clonedArray = [...array]
-
-  for (let index = clonedArray.length - 1; index > 0; index--) {
-    const randomIndex = Math.floor(Math.random() * (index + 1))
-    const original = clonedArray[index]
-
-    clonedArray[index] = clonedArray[randomIndex]
-    clonedArray[randomIndex] = original
-  }
-
-  return clonedArray
-}
-
-const attachEventListeners = () => {
-  document.addEventListener('click', event => {
-    const eventTarget = event.target
-    const eventParent = eventTarget.parentElement
-
-    if (eventTarget.className.includes('card') && !eventParent.className.includes('flipped')) {
-      flipCard(eventParent)
-    } else if (eventTarget.nodeName === 'BUTTON' && !eventTarget.className.includes('disabled')) {
-      startGame()
-    }
-  })
-}
-
 const startGame = () => {
   state.gameStarted = true
   selectors.start.classList.add('disabled')
@@ -116,7 +75,6 @@ const flipCard = card => {
       setTimeout(() => {
         checkWinGames()
       }, 600)
-
     }
 
     setTimeout(() => {
